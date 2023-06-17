99da5d36ec04cc8f92e5a2ba16be4d23105ac8ef
Line09x <line09.contact@gmail.com>
2023-06-17 10:13:38 -0700

    Updated

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,25 +10,26 @@
 
 int main(void)
 {
-    int a, b;
-    for (a = '0'; a < '9'; a++)
-    {
-        for (b = a + 1; b <= '9'; b++)
-        {
-            if (a != b)
-            {
-                putchar(a);
-                putchar(b);
+	int d, p;
 
-                if (a == '8' && b == '9')
+	for (d = '0'; d < '9'; d++)
+	{
+		for (p = d + 1; p <= '9'; p++)
+		{
+			if (p != d)
+			{
+				putchar(d);
+				putchar(p);
 
-                    continue;
+				if (d == '8' && p == '9')
+					continue;
 
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
 }
