ddfe6a25522ac957e40aed9687a9145853091a62
Line09x <line09.contact@gmail.com>
2023-06-21 06:02:38 -0700

    Updated

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,38 @@
 #include "main.h"
 
-
 /**
-* main - check the code
-*
-* Return: Always 0.
+* jack_bauer - prints every minute of the day of Jack Bauer
+* starting from 00:00 to 23:59
 */
-int main(void)
+
+void main(void)
 {
-times_table();
-return (0);
+int a, b, c;
+for (a = 0; a < 10; a++)
+{
+for (b = 0; b < 10; b++)
+{
+c = a * b;
+if (b == 0)
+{
+_putchar(c + '0');
+}
+
+if (c < 10 && b != 0)
+{
+ _putchar(',');
+_putchar(' ');
+_putchar(c + '0');
+}
+
+else if (c >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
+}
+}
+_putchar('\n');
+}
 }
